4607ee5bff8f1672c5eee66085400bc806c84f40
jjwachi23 <wachimoolang23@gmail.com>
2025-11-02 14:43:57 +0700

    Add: File test

---
+++ main.c
@@ -0,0 +1,30 @@
+// Libraries
+#include<stdio.h> // stdio = standrad input output
+#include<stdbool.h> // stdbool = standrad
+
+// Datatypes
+// int = 1234, -1234
+// char (charactor) = "", '' ("Hello C") 
+// bool (boolean) = validate value (true, false)
+// float = 3.14 (decimal point)
+
+// double = 3.145523 (more than 2 deciaml pint)
+// NULL = 
+
+// Variable name
+// 1. Datatypes 2. Variable name 3. Value default
+
+// Conditional
+// 1. if ()
+// 2. if...else ()
+// 3. if...else if ()
+// 4. if...else if...else ()
+
+// Loop
+
+int main() {
+    bool isAdult = true;
+    
+    printf("Hi, C");
+    return 0;
+}
