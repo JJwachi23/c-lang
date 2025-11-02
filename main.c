96f8deef1c10ac6adb7ca280e072d67eecb25fd9
jjwachi23 <wachimoolang23@gmail.com>
2025-11-02 15:06:48 +0700

    Edit: Comment code

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 // Libraries
-#include<stdio.h> // stdio = standrad input output
-#include<stdbool.h> // stdbool = standrad
+#include<stdio.h> // stdio = standard input output
+#include<stdbool.h> // stdbool = standard boolean
 
 // Datatypes
 // int = 1234, -1234
@@ -8,23 +8,28 @@
 // bool (boolean) = validate value (true, false)
 // float = 3.14 (decimal point)
 
-// double = 3.145523 (more than 2 deciaml pint)
-// NULL = 
+// double = 3.145523 (more than 2 decimal pint)
+// NULL = (Empty value)
 
 // Variable name
 // 1. Datatypes 2. Variable name 3. Value default
+// int x = 10;
+// char fullName = 'EDCO';
+// isYoung = flase;
 
 // Conditional
-// 1. if ()
-// 2. if...else ()
-// 3. if...else if ()
+// 1. if (if the conditional is TRUE)
+// 2. if...else (if gonna chose the conditional just one)
+// 3. if...else if (if want to check conditional more than 1)
 // 4. if...else if...else ()
 
-// Loop
-
+// function main() it's means the initial first of all the program in C
 int main() {
     bool isAdult = true;
     
+    // printf() it's means command to print show message or data
     printf("Hi, C");
+
+    // * return for end the function
     return 0;
 }
